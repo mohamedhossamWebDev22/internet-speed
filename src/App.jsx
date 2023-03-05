307c48433c2dba4d0c85aecc29eaafc1c951ac36
MohamedHossamElden <mohamedhossamelden2009@gmail.com>
2023-03-05 23:16:58 +0200

    add title

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import img1 from'./assets/img1.png';
 
 const App = () => {
   return (
     <>
-      
+      <div className="title">
+        <img src={img1}/>
+        <h1>Internet Speed</h1>
+      </div>
+
     </>
   );
 }
